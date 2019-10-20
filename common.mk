0cd2056c339310dae204fe1fb1c069d77ac5bbcd
Demon000 <demonsingur@gmail.com>
2019-10-20 20:18:30 +0200

    sm7125-common: set shipping api level
    Change-Id: I6fb3ccbdb534fc737888648bc780c2cdbe96070e

--- common.mk
+++ common.mk
@@ -16,3 +16,6 @@
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# API
+PRODUCT_SHIPPING_API_LEVEL := 29
