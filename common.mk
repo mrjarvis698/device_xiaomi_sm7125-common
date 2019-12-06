ae89202163d1521abf0d0e113ccb53a2214d3eac
Lokesh Gidra <lokeshgidra@google.com>
2019-12-06 14:00:16 -0800

    sm7125-common: Use 4GB dalvik-heap config
    Change-Id: I086d4a5c09634c3c7bd51ec5d08dcaa1551511b4

--- common.mk
+++ common.mk
@@ -20,6 +20,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit proprietary targets
 $(call inherit-product-if-exists, vendor/xiaomi/sm7125-common/sm7125-common-vendor.mk)
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
+
 # API
 PRODUCT_SHIPPING_API_LEVEL := 29
 
