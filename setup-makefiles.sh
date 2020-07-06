6d468b21f19bfaf7de8236dbd33f638feb0ba0c9
Demon000 <demonsingur@gmail.com>
2020-07-06 03:56:16 +0300

    sm7125-common: add support for excalibur

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -37,7 +37,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ROM_ROOT}" true
 
 # Copyright headers and guards
-write_headers "placeholder"
+write_headers "excalibur"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
