9faa122a37826e8f38c1b9dba4de1a38bd3ad625
Volodymyr Zhdanov <wight554@gmail.com>
2020-10-23 10:59:48 +0300

    sm7125-common: build our fingeprint service

--- common.mk
+++ common.mk
@@ -158,6 +158,9 @@ PRODUCT_PACKAGES += \
     fastbootd
 
 # Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sm7125
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
