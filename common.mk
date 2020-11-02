d2f178b295e0ba87b2d88fad4398ab07300cde05
Volodymyr Zhdanov <wight554@gmail.com>
2020-11-02 12:54:35 +0200

    sm7125-common: configure livedisplay
    * reboot issue fixed with https://review.lineageos.org/c/LineageOS/android_hardware_lineage_livedisplay/+/291215
    
    Revert "sm7125-common: Add config value for a11y color mode"
    
    This reverts commit 97a37101a3208557fc0f701dfa60ca3f8bb2e311.
    
    Revert "sm7125-common: Remove COLOR_MODE_SATURATED"
    
    This reverts commit 05e88c788f1b53edc2df14d7eb8d8e1a673d77f4.
    
    Revert "sm7125-common: Enable adaptive color mode"
    
    This reverts commit 656110c04a11caca5dc3121dd1864d758e51c29c.
    
    Revert "sm7125-common: Override config_availableColorModes"
    
    This reverts commit b9c2b988f01ac77ff476956458783c38f796645f.
    
    Revert "Revert "sm7125-common: add livedisplay support""
    
    This reverts commit a3a4c1544ca797b54bcfe7d6cb195b5ca493cec4.
    
    Revert "Revert "sm7125-common: enable default color mode""
    
    This reverts commit 8cba7668001eff8dd695dbf93db63b6b1f763b49.
    
    Partially revert "sm7125-common: add livedisplay support"

--- common.mk
+++ common.mk
@@ -245,6 +245,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_sm7125
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
+
 # Media
 PRODUCT_PACKAGES += \
     libmm-omxcore \
