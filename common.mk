871b48d0c3212d0fd1c5843a8acb46437367c819
Alexander Koskovich <zvnexus@outlook.com>
2020-11-18 15:10:31 -0700

    sm7125-common: Use the new AIDL QTI thermal HAL
    Change-Id: Ib9d983ad3eea58d840d2c5f8b42d5c6845b69906
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- common.mk
+++ common.mk
@@ -381,6 +381,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.qti
+
 # Ueventd
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc
