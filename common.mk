58551908f269b97a7bbd955cebe20c1b4cf2e723
Mr.Jarvis698 <dudamajaykumar59@gmail.com>
2021-05-06 18:02:32 +0000

    sm7125-common: Inherit proprietary targets

--- common.mk
+++ common.mk
@@ -17,5 +17,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Inherit proprietary targets
+$(call inherit-product-if-exists, vendor/xiaomi/sm7125-common/sm7125-common-vendor.mk)
+
 # API
 PRODUCT_SHIPPING_API_LEVEL := 29
