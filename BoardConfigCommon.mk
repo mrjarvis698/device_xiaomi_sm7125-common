e2bf41b7974b355b3d4df943dd3242cb56706eb2
Mr.Jarvis698 <dudamajaykumar59@gmail.com>
2021-05-20 16:26:13 +0000

    sm7125-common: rm overly and enable flag for ACS and IEEE80211AC
    This reverts commit c809a538b1e524f97b62bb57fa9a46155882f287.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -241,6 +241,8 @@ WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 TARGET_USES_ICNSS_QMI := true
+CONFIG_ACS := true
+CONFIG_IEEE80211AC := true
 
 # Inherit proprietary blobs
 -include vendor/xiaomi/sm7125-common/BoardConfigVendor.mk
